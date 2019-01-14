24dd3c3b36f0a7693dbe792fd8574fa0b6c4238e
SkrabanPatrik <spatrik880@gmail.com>
2019-01-14 18:59:40 +0100

    Dodana vrstica Hello Local.

--- Hello.cs
+++ Hello.cs
@@ -4,10 +4,6 @@ public class Hello
    public static void Main()
    {
       System.Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
 	  System.Console.WriteLine("Hello, Local!");
-=======
-      System.Console.WriteLine("Hello, GitHub!");
->>>>>>> 1378bb7bcb7351be99069185ef2f5233f238265c
    }
 }
